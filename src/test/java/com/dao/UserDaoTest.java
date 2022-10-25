8303cd6ff68231dafa298b350571caa417c629d6
SuInWoo <wbo1026@naver.com>
2022-10-25 13:24:02 +0900

    try-catch-finally 추가

--- src/test/java/com/dao/UserDaoTest.java
+++ src/test/java/com/dao/UserDaoTest.java
@@ -7,12 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
