c6a07f1a3e4db2c0690677cee1f3ad5ab721283f
SuInWoo <wbo1026@naver.com>
2022-10-25 11:45:24 +0900

    insert spring - spring-test in gradle - spring-starter in gradle

--- src/main/java/com/dao/ConnectionMaker.java
+++ src/main/java/com/dao/ConnectionMaker.java
@@ -4,5 +4,5 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public interface ConnectionMaker {
-    public Connection getConnection() throws SQLException;
+    Connection getConnection() throws SQLException;
 }
